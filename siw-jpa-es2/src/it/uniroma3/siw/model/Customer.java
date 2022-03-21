9181c8cfd4e4497b20305be3f7768555c6a86aa8
drew <43808240+drew458@users.noreply.github.com>
2022-03-21 14:32:39 +0100

    step-one

---
+++ siw-jpa-es2/src/it/uniroma3/siw/model/Customer.java
@@ -0,0 +1,120 @@
+package it.uniroma3.siw.model;
+
+import java.time.LocalDate;
+import javax.persistence.*;
+
+@Entity
+@NamedQuery(name = "findAllCustomers", query = "SELECT c FROM Customer c")
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String firstName;
+	
+	@Column(nullable = false)
+	private String lastName;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String phoneNumber;
+	
+	@Column(nullable = false)
+	private LocalDate dateOfBirth;
+	
+	@Column(nullable = false)
+	private LocalDate registrationDate;
+	
+	public Customer() {
+		
+	}
+	
+	public Customer(Long id, String firstName, String lastName, String email, String phoneNumber,
+			LocalDate dateOfBirth, LocalDate registrationDate) {
+		this.id=id;
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.email=email;
+		this.phoneNumber=phoneNumber;
+		this.dateOfBirth=dateOfBirth;
+		this.registrationDate=registrationDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public LocalDate getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(LocalDate registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Customer that = (Customer) obj;
+		return this.getId().equals(that.getId());
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.id.hashCode();
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+}
