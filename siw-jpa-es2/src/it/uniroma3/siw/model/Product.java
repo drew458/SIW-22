4f49fb71e87c1ca5fda0db7d5e675daf0a37f472
drew <43808240+drew458@users.noreply.github.com>
2022-03-21 17:30:45 +0100

    es2 - getters&setters fix

--- siw-jpa-es2/src/it/uniroma3/siw/model/Product.java
+++ siw-jpa-es2/src/it/uniroma3/siw/model/Product.java
@@ -23,5 +23,62 @@ public class Product {
 	
 	@ManyToMany(mappedBy = "products")
 	private List<Provider> providers;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	public List<Provider> getProviders() {
+		return providers;
+	}
+
+	public void setProviders(List<Provider> providers) {
+		this.providers = providers;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Product that = (Product)obj;
+		return this.id.equals(that.getId());
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.id.hashCode();
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
 	
 }
